059ad1a3a794687277b1a01aa793094a9e3190a5
gabriellguimaraes <gabriel.alg@usp.br>
2026-04-30 12:45:16 -0300

    cleaned some code

--- src/bayes_file_generator/__init__.py
+++ src/bayes_file_generator/__init__.py
@@ -1,3 +1,12 @@
 """Bayes file generator package."""
 
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+from .cli import run_generation
+
+try:
+    __version__ = version("bayes-file-generator")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+
+__all__ = ["run_generation", "__version__"]
